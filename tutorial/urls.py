16cc1ce84e49da1856e527e234f163a914800c84
Denis <den3900@mail.ru>
2020-05-16 18:29:50 +0300

    DRF Tutorial 4

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from tutorial.quickstart import views
 from rest_framework import routers
+from django.conf.urls import include
 
 
 router = routers.DefaultRouter()
+
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
@@ -16,4 +18,8 @@ urlpatterns = [
     #     include('rest_framework.urls', namespace='rest_framework')
     # ),
     path('', include('snippets.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
